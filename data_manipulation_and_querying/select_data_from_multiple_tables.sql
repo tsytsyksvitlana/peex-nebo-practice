c9b3b7cbb477862b6f437e79bfe49c1e6449fcbd
Svitlana Tsytsyk <TSYTSYK.svetlana@gmail.com>
2025-12-28 21:14:55 +0200

    add sql examples

---
+++ data_manipulation_and_querying/select_data_from_multiple_tables.sql
@@ -0,0 +1,38 @@
+-- UNION ALL: Show all transitions from trial to paid plans, including duplicates
+SELECT customer_id, plan_id, start_date
+FROM foodie_fi.subscriptions
+WHERE plan_id = 0
+UNION ALL
+SELECT customer_id, plan_id, start_date
+FROM foodie_fi.subscriptions
+WHERE plan_id IN (1,2,3);
+
+
+-- UNION: List all unique users who ever had trial or pro plans
+SELECT customer_id
+FROM foodie_fi.subscriptions
+WHERE plan_id = 0
+UNION
+SELECT customer_id
+FROM foodie_fi.subscriptions
+WHERE plan_id IN (2,3);
+
+
+-- INTERSECT: Find users who had trial and later subscribed to a paid plan
+SELECT customer_id
+FROM foodie_fi.subscriptions
+WHERE plan_id = 0
+INTERSECT
+SELECT customer_id
+FROM foodie_fi.subscriptions
+WHERE plan_id IN (1,2,3);
+
+
+-- EXCEPT: Find users who had trial but never upgraded to a paid plan
+SELECT customer_id
+FROM foodie_fi.subscriptions
+WHERE plan_id = 0
+EXCEPT
+SELECT customer_id
+FROM foodie_fi.subscriptions
+WHERE plan_id IN (1,2,3);
---
+++ data_manipulation_and_querying/type_conversion.sql
@@ -0,0 +1,21 @@
+-- Select campaign_name, start_date, and convert start_date to DATE type from campaign_identifier table.
+SELECT
+    campaign_name,
+    start_date,
+    CAST(start_date AS DATE) AS campaign_start_date
+FROM clique_bait.campaign_identifier;
+
+
+-- for mssql
+SELECT
+    campaign_name,
+    CONVERT(start_date, DATE) AS start_date_converted
+FROM clique_bait.campaign_identifier;
+
+
+SELECT 
+    campaign_name,
+    TRY_CONVERT(DATE, '2025-12-28') AS safe_date,
+    TRY_CONVERT(INT, '123') AS safe_int,
+    TRY_PARSE('12/31/2025' AS DATE USING 'en-US') AS us_date,
+    TRY_PARSE('31.12.2025' AS DATE USING 'de-DE') AS de_date;
